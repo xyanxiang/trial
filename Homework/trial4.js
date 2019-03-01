b6ddbe9e4614f0d691da5a0643a88c4a998c7614
xiangyan <811029742@qq.com>
2019-03-01 19:01:40 +0800

    add files

--- Homework/trial4.js
+++ Homework/trial4.js
@@ -1,28 +1,11 @@
 // initialize the page when loading finished;
 
-window.addEventListener('load',initialize);
+window.onload = initialize;
 
-function initialize(){
-  if(document.getElementById){
-    var form = document.getElementById("theForm");
-    if(form){
-      form.addEventListener('submit',checkForm);
-    }
+var initialize = function(){
+  if(document.forms){
+    var 
   }
-}
-function checkForm(evt){
-  if(!evt) evt = window.event;
-
-  for(var i = 0; i< this.elements.length; i++){
-    if(this.elements[i].type == "text" && this.elements[i].value== ""){
-      alert("fill all fields");
-      if(evt.preventDefault) evt.preventDefault();
-      return false;
-    }
-  }
-return true;
-
-   
 }
 
 
