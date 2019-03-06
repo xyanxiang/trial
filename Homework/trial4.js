d614622e4855685265869672ed4a74cc99b62ff7
xiangyan <811029742@qq.com>
2019-03-06 20:56:30 +0800

    add files

--- Homework/trial4.js
+++ Homework/trial4.js
@@ -2,11 +2,53 @@
 
 window.onload = initialize;
 
-var initialize = function(){
-  if(document.forms){
-    var 
+function initialize(){
+  if(document.getElementById){
+    var oEntry = document.getElementById("entry");
+    var oButton = document.getElementById("do-it");
+    if(oEntry&&oButton){
+      oButton.addEventListener("click",doIt);
+    }
   }
 }
+
+function doIt(){
+  var oEntry = document.getElementById('entry');
+  oEntry.value = oEntry.value.initialCaps();
+}
+
+String.prototype.initialCaps = function(){
+  var sText = this.toString();
+  console.log(sText);
+
+  var aWords = sText.split('');
+  console.log(aWords);
+  var sInitial = aWords[0].charAt(0);
+    var sReminder = aWords[0].substr(1);
+    aWords[i] = sInitial.toUpperCase() + sReminder.toLowerCase();
+  // for(var i = 0; i<aWords.length;i++){
+  //   var sInitial = aWords[i].charAt(0);
+  //   var sReminder = aWords[i].substr(1);
+  //   aWords[i] = sInitial.toUpperCase() + sReminder.toLowerCase();
+  // } 
+ return aWords.join(' ');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
